40900eb1852f587681699a220e6b7d6754db034c
aknietsagitzhan <ramazan.akniet08@icloud.com>
2026-02-02 08:42:53 +0500

    initial commit

---
+++ lab2-ejudge/209.py
@@ -0,0 +1,15 @@
+n = int(input())
+a = list(map(int, input().split()))
+mx = a[0]
+mn = a[0]
+for i in a:
+    if i > mx:
+        mx = i
+    if i < mn:
+        mn = i
+
+for i in a:
+    if i == mx:
+        print(mn, end=" ")
+    else: 
+        print(i, end=" ")
