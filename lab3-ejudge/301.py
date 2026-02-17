5f6c1a4e697ce0803ec007c9f68001a0c108ebc7
aknietsagitzhan <ramazan.akniet08@icloud.com>
2026-02-17 14:29:03 +0500

    Initial commit

--- lab3-ejudge/301.py
+++ lab3-ejudge/301.py
@@ -1,11 +1,13 @@
-a = input().strip()
-ans = True
-for i in a:
-    i = int(i)
-    if i % 2 != 0:
-        ans = False
-        break
-if ans:
-    print("Valid")
-else:
-    print("Not valid")
+a = input()
+def valid(a):
+    ans = True
+    for i in a:
+        i = int(i)
+        if i % 2 != 0:
+            ans = False
+            break
+    if ans:
+        print("Valid")
+    else:
+        print("Not valid")
+valid(a)
